8b3829236cac1fe5f54982fcf7d757a2229889b3
binhnv6 <binhnv6@vng.com.vn>
2026-06-19 14:44:40 +0700

    feat: update

--- extension/config.js
+++
@@ -1,2 +0,0 @@
-// Agent endpoint — update this when redeploying to a new runtime
-const AGENT_BASE_URL = 'https://endpoint-f0b1c066-9a31-44c0-b18a-866205de4ee2.agentbase-runtime.aiplatform.vngcloud.vn';
